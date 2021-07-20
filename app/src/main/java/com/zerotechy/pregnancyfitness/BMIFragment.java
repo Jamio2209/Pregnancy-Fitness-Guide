1990910c17f52c25b0baa80fb224f5cdf021297e
Jamio2209 <53439845+Jamio2209@users.noreply.github.com>
2021-07-21 01:45:38 +0600

    BMI Page completed with all the features

--- app/src/main/java/com/zerotechy/pregnancyfitness/BMIFragment.java
+++ app/src/main/java/com/zerotechy/pregnancyfitness/BMIFragment.java
@@ -7,6 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Formatter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +20,15 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class BMIFragment extends Fragment {
+
+    TextView height,weight,result,yourbmi;
+    ImageView button;
+    SeekBar sheight,sweight;
+    double heightvalue,weightvalue;
+    boolean repeat=false;
+
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +74,109 @@ public class BMIFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_b_m_i, container, false);
+        View v=inflater.inflate(R.layout.fragment_b_m_i,container,false);
+
+        height=(TextView)v.findViewById(R.id.heightamount);
+        weight=(TextView)v.findViewById(R.id.weightamount);
+        yourbmi=(TextView)v.findViewById(R.id.yourbmi);yourbmi.setVisibility(View.INVISIBLE);
+        result=(TextView)v.findViewById(R.id.result);
+         result.setVisibility(View.INVISIBLE);
+        sheight=(SeekBar)v.findViewById(R.id.heightbar);
+        sweight=(SeekBar)v.findViewById(R.id.weighttbar);
+        button=(ImageView)v.findViewById(R.id.bmigobtn);
+
+        sheight.setMax(250);
+        sheight.setProgress(0,true);
+        sweight.setProgress(0);
+        heightvalue=(double) sheight.getProgress();
+        weightvalue=(double)sweight.getProgress();
+        sheight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                height.setText(String.valueOf(progress)+" cm");
+                heightvalue=(double) progress/100;
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+            sweight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    weight.setText(String.valueOf(progress)+" kg");
+                    weightvalue=(double)progress;
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(!repeat){
+                        try {
+                        heightvalue=heightvalue*heightvalue;
+                        double newresult=weightvalue/heightvalue;
+                        Formatter formatter = new Formatter();
+                        formatter.format("%.2f", newresult);
+                        result.setText(String.valueOf(formatter));
+                        result.setVisibility(View.VISIBLE);
+                        yourbmi.setVisibility(View.VISIBLE);
+//                        Toast.makeText(getContext(), String.valueOf(heightvalue)+"  "+String.valueOf(weightvalue), Toast.LENGTH_LONG).show();
+                    }catch (Exception e){
+                        e.printStackTrace();
+
+                    }
+
+                        sheight.setEnabled(false);
+                        sweight.setEnabled(false);
+                        button.setImageResource(R.drawable.repeatbtnbmi);
+                        repeat=true;
+
+                    }
+                    else {
+                        sheight.setEnabled(true);
+                        sweight.setEnabled(true);
+                        sheight.setProgress(0,true);
+                        sweight.setProgress(0,true);
+                        button.setImageResource(R.drawable.bmi_go);
+                        result.setVisibility(View.INVISIBLE);
+                        yourbmi.setVisibility(View.INVISIBLE);
+                        repeat=false;
+                    }
+
+
+
+
+                    }
+
+
+
+
+
+            });
+
+
+
+
+        return v;
     }
 }
